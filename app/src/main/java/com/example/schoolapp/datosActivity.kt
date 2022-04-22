4b026c17bf3c6ba29f092ac7a48461a7bb840c2f
Edson-MT1 <al061747@uacam.mx>
2022-04-22 02:05:41 -0500

    carga de proyecto

---
+++ app/src/main/java/com/example/schoolapp/datosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.schoolapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class datosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos)
+    }
+}
