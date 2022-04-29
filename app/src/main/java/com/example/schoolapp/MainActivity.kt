6db54b27a50be1f81797a3f1f08127c11d9eac8f
Edson-MT1 <al061747@uacam.mx>
2022-04-28 21:56:08 -0500

    Implementación de nevegación entre interfaces

--- app/src/main/java/com/example/schoolapp/MainActivity.kt
+++ app/src/main/java/com/example/schoolapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.schoolapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView3.setOnClickListener{
+            val intent:Intent = Intent(this, DatosActivity::class.java)
+            startActivity(intent)
+        }
+
+        textView2.setOnClickListener{
+            val intent:Intent = Intent(this, PagosActivity::class.java)
+            startActivity(intent)
+        }
+
+        textView4.setOnClickListener{
+            val intent:Intent = Intent(this, BitacoraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
